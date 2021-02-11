1dc0dd45634af5ad220bc2c9b566b5b80f86f5a5
rachelduret <chuncheung.ku@gmail.com>
2021-02-11 16:25:58 +0100

    fetch dispaly goods

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
 // add goods in html
-window.onload = function request(url, callback){
-    let request = new XMLHttpRequest();
-              request.onreadystatechange = function(){
-                  if (request.readyState == 4 && request.status ==200){
-                      let dataArray = JSON.parse(this.responseText);
-                      function displayTeddies(href, src, title, price){
-                        let box = document.getElementById('teddies');
-                        let a = document.createElement('a');
-                        let div = document.createElement('div');
-                        let img = document.createElement('img');
-                        let h2 = document.createElement('h2')
-                        let p = document.createElement('p')
-                        let i = document.createElement('i')
-                        a.href = href;
-                        div.className = "teddy";
-                        img.src = src;
-                        h2.innerText = title;
-                        p.innerText = price ;
-                        i.className ="fas fa-euro-sign"
-                        box.appendChild(a);
-                        a.appendChild(div);
-                        div.appendChild(img);
-                        div.appendChild(h2);
-                        div.appendChild(p);
-                        p.appendChild(i)
-                        console.log(box)
-                                               
-                    }
-                    displayTeddies('./produits/norbert.html', dataArray[0].imageUrl, dataArray[0].name, dataArray[0].price);
-                    displayTeddies('./produits/arnold.html',dataArray[1].imageUrl, dataArray[1].name, dataArray[1].price);
-                    displayTeddies('./produits/lenny_and_carl.html',dataArray[2].imageUrl, dataArray[2].name, dataArray[2].price);
-                    displayTeddies('./produits/gustav.html',dataArray[3].imageUrl, dataArray[3].name, dataArray[3].price);
-                    displayTeddies('./produits/garfunkel.html',dataArray[4].imageUrl, dataArray[4].name, dataArray[4].price);
-                              
-                  }
-              }
-              request.open("get", "http://localhost:3000/api/teddies");
-              request.send();
-
-}
-
-
-
-//create page norbert.html
-
-
-
-
-
+let url = fetch('http://localhost:3000/api/teddies');
+url.then(function(data){
+    return data.json();
+   
+}).then(function(dataArray){
+    console.log(dataArray);
+    function displayTeddies(id, src, title, price){
+        let box = document.getElementById('teddies');
+        let div = document.createElement('div');
+        let img = document.createElement('img');
+        let h2 = document.createElement('h2')
+        let p = document.createElement('p')
+        let i = document.createElement('i')
+        let btn = document.createElement('button');
+        div.className = "teddy";
+        img.src = src;
+        h2.innerText = title;
+        p.innerText = price ;
+        i.className ="fas fa-euro-sign"
+        btn.innerText = 'Voir plus'
+        btn.id =id;
+        btn.className = 'btn';
+        box.appendChild(div);
+        div.appendChild(img);
+        div.appendChild(h2);
+        div.appendChild(p);      
+        p.appendChild(i)
+        p.appendChild(btn);
+        console.log(box)
+                               
+    }
+    displayTeddies( dataArray[0].name, dataArray[0].imageUrl, dataArray[0].name, dataArray[0].price);
+    displayTeddies(dataArray[1].name,dataArray[1].imageUrl, dataArray[1].name, dataArray[1].price);
+    displayTeddies(dataArray[2].name,dataArray[2].imageUrl, dataArray[2].name, dataArray[2].price);
+    displayTeddies(dataArray[3].name,dataArray[3].imageUrl, dataArray[3].name, dataArray[3].price);
+    displayTeddies( dataArray[4].name,dataArray[4].imageUrl, dataArray[4].name, dataArray[4].price);
+       
+})
+.catch(function(error){
+    alert('error');
+    
+});

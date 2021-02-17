35b2e633fc4b97bc0e0df9fdbf451b1eca59cdf1
rachelduret <chuncheung.ku@gmail.com>
2021-02-17 11:54:50 +0100

    add total prix

--- panier.js
+++ panier.js
@@ -6,43 +6,57 @@ function displayPanier( src, title, id, colors, quantité, price){
     let div = document.createElement('div');
     let img = document.createElement('img');
     let name = document.createElement('h2')
-    let idNumber = document.createElement('p')
+    //let idNumber = document.createElement('p')
     let color = document.createElement('p')
     let totalQte = document.createElement('p')
     let prix = document.createElement('p')
     let i = document.createElement('i')
     let btnDel = document.createElement('button');
+   
     
     
     div.className = "teddy";
     img.src = src;
     name.innerText = title;
-    idNumber.innerText ='ref:'+ id;
+    //idNumber.innerText ='ref:'+ id;
     color.innerText = colors;
     totalQte.innerText = quantité;
     prix.innerText = price;
     i.className = "fas fa-euro-sign"
     btnDel.className = 'delBtn'
-    btnDel.id = id;
     btnDel.innerText = 'Supprimer'
-    
-    
    
     panier.appendChild(div);
     div.appendChild(img);
     div.appendChild(name);
-    name.appendChild(idNumber);
+    //name.appendChild(idNumber);
     div.appendChild(color)
     div.appendChild(totalQte);
     div.appendChild(prix);      
     prix.appendChild(i)
     div.appendChild(btnDel);
+   
  
                            
 }
 for(let i in panierLists){
     displayPanier( panierLists[i].img, panierLists[i].name,panierLists[i].id,panierLists[i].color,panierLists[i].quantité,panierLists[i].price);
 }
+
+ // count total price
+ let sum = 0;
+ for( let i=0; i<panierLists.length; i++){
+     sum += panierLists[i].price 
+ }
+ let totalPrix = document.createElement('h3');
+ let i = document.createElement('i');
+ i.className = "  fas fa-euro-sign"
+ totalPrix.innerText = 'Total Prix:   ' + sum ;
+ totalPrix.appendChild(i);
+ panier.appendChild(totalPrix);
+
+ console.log(panier)
+
 function deletGood(){
     let btns = document.querySelectorAll('.delBtn');
     
@@ -55,8 +69,8 @@ function deletGood(){
             let removed = parent.removeChild(delDiv);
             removed===removed;
              panierLists.splice(i,1);
-             console.log(panierLists)
-            console.log(JSON.stringify(panierLists));
+             //console.log(panierLists)
+           // console.log(JSON.stringify(panierLists));
             localStorage.removeItem('panierLists')
             localStorage.setItem('panierLists',JSON.stringify(panierLists));
             
